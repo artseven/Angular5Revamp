7ed2b686e2eff30532cc9fd2cf3400bebce1fb52
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-04-25 15:49:04 -0400

    adding modal

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectionStrategy, ViewChild,TemplateRef } from '@angular/core';
 import { Subject } from 'rxjs/Subject'
 import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent } from 'angular-calendar';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'calendar-demo',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -10,16 +11,34 @@ import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent } fr
 })
 
 export class DemoComponent {
+
+  @ViewChild('modalContent') modalContent: TemplateRef<any>;
+
   view: string = 'month';
   popContent = 'Blah';
+  modalData: {
+    action: string,
+    event: CalendarEvent
+  }
   viewDate = new Date();
   events = [];
   nextDate = new Date('April 26, 2018');
-  constructor() {
-
+  constructor(private modal: NgbModal) {
     this.events.push({start: this.viewDate, end: this.viewDate, title: 'TWO DAY EVENT'});
     this.events.push({start: this.viewDate, end: this.nextDate, title: 'ONE DAY EVENT'});
-    this.events.push
+  }
 
+  actions: CalendarEventAction[] = [
+    {
+      label: '<i class="fa fa-fw fa-pencil"></i>',
+      onClick: ({event}: {event: CalendarEvent}): void => {
+        this.handleEvent('Edited', event);
+      }
+    }
+  ]
+
+  handleEvent(action: string, event: CalendarEvent): void {
+    this.modalData = { event, action };
+    this.modal.open(this.modalContent, { size: 'lg' });
   }
 }
