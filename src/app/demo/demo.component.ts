e94e7abc26f7a4d42dd7f05f5b2fde00a3d53d10
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-04-25 14:45:45 -0400

    calendar view is in place, working on events

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, ChangeDetectionStrategy, ViewChild,TemplateRef } from '@angular/core';
+import { Subject } from 'rxjs/Subject'
+import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent } from 'angular-calendar';
 @Component({
   selector: 'calendar-demo',
-  templateUrl: 'demo.component.html'
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: 'demo.component.html',
+  styleUrls: ['demo.styles.css']
 
 })
 
@@ -10,4 +13,10 @@ export class DemoComponent {
   view: string = 'month';
   viewDate = new Date();
   events = [];
+  nextDate = new Date('April 26, 2018');
+  constructor() {
+    this.events.push({start: this.viewDate, end: this.viewDate});
+    this.events.push({start: this.viewDate, end: this.nextDate});
+
+  }
 }
