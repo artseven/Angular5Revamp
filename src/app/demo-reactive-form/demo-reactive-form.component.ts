ec1fb4bb583b09f4266c41486d3ceead3e59bb07
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-04-26 16:37:53 -0400

    reactive form

--- src/app/demo-reactive-form/demo-reactive-form.component.ts
+++ src/app/demo-reactive-form/demo-reactive-form.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-demo-reactive-form',
   templateUrl: './demo-reactive-form.component.html',
-  styleUrls: ['./demo-reactive-form.component.css']
+  styleUrls: ['./demo-reactive-form.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DemoReactiveFormComponent implements OnInit {
 
+  nameForm: FormGroup
   constructor() { }
 
   ngOnInit() {
+    this.nameForm = new FormGroup({
+      firstname:  new FormControl('', {
+        validators: Validators.required,
+        updateOn: 'change'
+      }),
+      lastname: new FormControl('', {
+        validators: Validators.required,
+        updateOn: 'change'
+      })
+
+    });
+
   }
 
 }
